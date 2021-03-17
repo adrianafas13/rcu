3f85c17e899c9180f2cfcccb63391af42965f8d1
Adriana <adrianafas13@hotmail.com>
2021-03-17 15:22:44 -0400

    maquetacion del home parte 1

--- resources/views/prueba.blade.php
+++ resources/views/prueba.blade.php
@@ -1,20 +1,93 @@
 <!doctype html>
+
 <html lang="en">
   <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+
+    <meta charset="utf-8"> 
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Revista Científica Unimar</title>
     <link rel="icon" href="{{asset('img/unimar-logo.png')}}" type="image/png"/>
-    <link rel="stylesheet" href="{{asset('css/style.css')}}">
-  </head>
-  <header>
-    <h1>esta es una prueba</h1>
-  </header>
-  <body>
-    <h4>esta es una prueba</h4>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="{{asset('plugins/fontawesome-free/css/all.min.css') }}">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js">
+    
+  </head>
+
+  <body>
+    <div class="container-fluid">
+      <div class="header">
+        <div class="row">
+
+          <div class="col">
+            <div class="logotipo">
+              <img src="{{asset('img/unimar-logo.png')}}" alt="logo" width="80px" height="auto">
+            </div>
+          </div>
+
+          <div class="col">
+            <ul id="iconos">
+
+              <li class="nav-item">
+                <div class="dropdown">
+                  <button class="btn dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <i class="fas fa-user-circle fa-2x"></i>
+                  </button>
+                  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                    <a class="dropdown-item">Iniciar Sesion</a>
+                    <a class="dropdown-item">Registrarse</a>
+                  </div>
+                </div>
+              </li>
+
+            </ul>
+          </div>
+
+        </div>
+      </div>
+
+      <div class="topnav" id="myTopnav">
+        <button class="dropdown">Lineas de Investigación
+          <i class="fa fa-caret-down"></i>
+        </button>
+
+          <div class="dropdown-content">
+            <a href="#">Administración</a>
+            <a href="#">Arte</a>
+            <a href="#">Idiomas</a>
+            <a href="#">Informatica</a>
+            <a href="#">Derecho</a>
+            <a href="#">Gerencia</a>
+            <a href="#">Historia</a>
+            <a href="#">Salud</a>
+          </div>
+
+        </div>
+
+        <a href="Artículos"></a>
+        <a href="Autores"></a>
+        <a href="Información"></a>
+
+        <div class="search-container">
+          <form type="get" action="#">
+            <input type="text" name="query" placeholder="Serach...">
+              <button type="submit">
+                <i class="fa fa-search"></i>
+              </button>
+          </form>
+        </div>
+
+        <a href="#" class="icon"><i class="fa fa-bars"></i></a>
+
+      </div>
+
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
   </body>
+
 </html>
